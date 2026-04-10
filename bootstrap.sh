aeb06bb175aa35d0c28e5865b5a7d9874cecda6f
Raul <raulrc0227@gmail.com>
2026-04-10 20:44:54 +0000

    Install postgres, redis, and clone repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,6 +21,31 @@ fi
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm install 24
-nvm alias default 24 # Makes node 24 the default version
+nvm alias default 24 
+
+# Install and setup postgresql
+echo "=== Installing Postgresql ==="
+sudo apt install -y postgresql postgresql-contrib
+sudo systemctl enable --now postgresql
+
+sudo -u postgres psql -c "CREATE ROLE pretzel_user WITH LOGIN PASSWORD 'pretzel_password';" 2>/dev/null || true
+sudo -u postgres psql -c "CREATE DATABASE pretzel_shop OWNER pretzel_user;" 2>/dev/null || true
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE pretzel_shop TO pretzel_user;" 2>/dev/null || true
+
+# Install redis
+echo "=== Installing Redis ==="
+sudo apt install -y redis-server
+sudo systemctl enable --now redis-server
+
+# Install NGINX
+echo "=== Installing NGINX ==="
+sudo apt install -y nginx
+sudo systemctl enable --now nginx
+
+# Clone repo
+if [ ! -d "$APP_DIR" ]; then
+  git clone "$REPO_URL" "$APP_URL"
+fi
+
 
 echo $APP_DIR
