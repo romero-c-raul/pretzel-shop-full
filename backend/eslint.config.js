1dc1b822e1390c83ec277769431570c64c6991ec
Raul <raulrc0227@gmail.com>
2026-04-25 19:45:30 +0000

    Install eslint on backend, define eslint config file

---
+++ backend/eslint.config.js
@@ -0,0 +1,15 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+  { ignores: ['node_modules', 'dist'] },
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+  },
+]
